fde66ddc2866b19252326d8cbd8bf6fbd33c3c8c
Balázs Kiss <dev@balazskiss.name>
2024-03-28 10:19:53 +0100

    1.9.0 release

--- Package.swift
+++ Package.swift
@@ -19,33 +19,43 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SwitchboardSDK",
-            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.8.1/ios/SwitchboardSDK.xcframework.zip",
-            checksum: "7f53e92e7df8653a44ed08bee80186910d7e2d00bff33ddd439aeab5222390a1"
+            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.9.0/ios/SwitchboardSDK.xcframework.zip",
+            checksum: "e090c917faf106a3e7088694f2f329c3c3e79cb2c5e8b85c2ac5e5bfe1969c5b"
         ),
         .binaryTarget(
             name: "SwitchboardUI",
-            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.8.1/ios/SwitchboardUI.xcframework.zip",
-            checksum: "cdad7714731007114d2d466460de8ffebb74157557446bed2819c5ed15606678"
+            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.9.0/ios/SwitchboardUI.xcframework.zip",
+            checksum: "2cf844b4ad96aae5f88ea978cefc6dd0a4aef17ab9b2caf876d25c02bcc3ce9b"
         ),
         .binaryTarget(
             name: "SwitchboardAudioEffects",
-            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.8.1/ios/SwitchboardAudioEffects.xcframework.zip",
-            checksum: "5cd0edfa5ba7120ea9477616efab3880b295051bd20af3f5f62c7d7499aec1a7"
+            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.9.0/ios/SwitchboardAudioEffects.xcframework.zip",
+            checksum: "87fdd13c1ccae5445b67a4011e46cddb82ec3d522cd8aa79be514f128906b4a7"
         ),
         .binaryTarget(
             name: "SwitchboardAgora",
-            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.8.1/ios/SwitchboardAgora.xcframework.zip",
-            checksum: "47752b1988f14b0612f26e7ace391995aae344dd8393ffbb7013859e0750fa27"
+            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.9.0/ios/SwitchboardAgora.xcframework.zip",
+            checksum: "161bdbee0eeab69a4c70eec209c9334b3c3842a944614832a47b0bf5f40edd51"
         ),
         .binaryTarget(
             name: "SwitchboardRNNoise",
-            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.8.1/ios/SwitchboardRNNoise.xcframework.zip",
-            checksum: "da9fbe0cf778ce2744924e1b4f207d1c91a2fdbd2ed92f4201155eef9016f574"
+            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.9.0/ios/SwitchboardRNNoise.xcframework.zip",
+            checksum: "57fc5f6c667c664622b814d1742b4eaab3a430f1e4c3c0a6d46f1c4a3986e16b"
         ),
         .binaryTarget(
             name: "SwitchboardSuperpowered",
-            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.8.1/ios/SwitchboardSuperpowered.xcframework.zip",
-            checksum: "fab5460f94e24ac60cf6a5bb53da8b49cfdceb849188815aa603037ac661eb9b"
+            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.9.0/ios/SwitchboardSuperpowered.xcframework.zip",
+            checksum: "95f9753e5a10b6fe279c8f00f9622e39eb3af13dc06f26c79212b84ea5d083f6"
+        ),
+        .binaryTarget(
+            name: "SwitchboardAmazonIVSLowLatency",
+            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.9.0/ios/SwitchboardAmazonIVSLowLatency.xcframework.zip",
+            checksum: "4fafe5c4968055c0c996a056f96da5d0734b0b78fcd3366ddc66d979ebe7c464"
+        ),
+        .binaryTarget(
+            name: "SwitchboardAmazonIVSRealTime",
+            url: "https://switchboard-sdk-public.s3.amazonaws.com/builds/release/1.9.0/ios/SwitchboardAmazonIVSRealTime.xcframework.zip",
+            checksum: "145065fa84b8847cf356b0fbe0af8f2ef2cef1175199908ab6673a845f219d24"
         ),
     ]
 )
---
+++ README.md
@@ -0,0 +1,17 @@
+# Switchboard SDK iOS
+
+This repository provides the `Package.swift` file that can be used in Xcode iOS projects to fetch the Switchboard SDK.
+
+## License
+
+See the LICENSE.txt file for details.
+
+## Contact
+
+If you have any questions or need assistance with the Switchboard SDK, please don't hesitate to contact us:
+
+Email: [hello@synervoz.com](mailto:hello@synervoz.com)
+
+Website: [https://docs.switchboard.audio](https://docs.switchboard.audio)
+
+Happy coding! 🚀
